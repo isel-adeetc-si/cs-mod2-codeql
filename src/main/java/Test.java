7a38b935260847c3fd02163aac314fd3429d78e3
José Simão <jsimao@cc.isel.ipl.pt>
2021-11-17 23:00:31 +0000

    throws exception

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,11 @@
 class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // BAD: user input might include special characters such as ampersands
         {
             String latlonCoords = args[1];
             Runtime rt = Runtime.getRuntime();
             Process exec = rt.exec("cmd.exe /C latlon2utm.exe " + latlonCoords);
+            exec.waitFor();
         }
         // GOOD: use an array of arguments instead of executing a string
         {
@@ -13,6 +14,7 @@ class Test {
             Process exec = rt.exec(new String[] {
                 "c:\\path\\to\\latlon2utm.exe",
                 latlonCoords });
+            exec.waitFor();
         }
     }
 }
